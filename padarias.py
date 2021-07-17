66473c52209b84e700afbada05b1a998430d2790
Vinicius George <86753434+mikloamir@users.noreply.github.com>
2021-07-17 06:26:51 -0300

    Add files via upload
    Adicionando padarias

---
+++ padarias.py
@@ -0,0 +1,33 @@
+padarias = {
+    'super_pao' : {
+        'distancia': 2,
+        'pão de sal': 5,
+        'baguete': 7,
+        'manteiga': 10,
+        'aumento_fds': True
+    },
+
+    'vale_do_trigo' : {
+        'distancia': 5,
+        'pão de sal': 4,
+        'baguete': 9,
+        'manteiga': 9,
+        'aumento_fds': True
+    },
+
+    'padaria_do_tunico' : {
+        'distancia': 0.5,
+        'pão de sal': 4.5,
+        'baguete': 8,
+        'manteiga': 9.5,
+        'aumento_fds': True
+    },
+
+    'panificadora_verona' : {
+        'distancia': 9,
+        'pão de sal': 6,
+        'baguete': 9,
+        'manteiga': 3,
+        'aumento_fds': False
+    }
+}
